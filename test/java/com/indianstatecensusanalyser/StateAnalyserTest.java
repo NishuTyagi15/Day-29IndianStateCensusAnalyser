442c3d0c899e8cfa5b3f35a8b955b9b2c599f730
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-28 21:27:35 +0530

    Added:Welcome Message

---
+++ test/java/com/indianstatecensusanalyser/StateAnalyserTest.java
@@ -0,0 +1,7 @@
+package com.indianstatecensusanalyser;
+
+public class StateAnalyserTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Indian State Census Analyser Program");
+    }
+}
